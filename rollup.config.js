84c2e3afc74b12895a5d5a08b81f49bcb7048c27
disarticulate <eric@lorefolk.com>
2019-09-29 13:26:02 -0500

    broke everything

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import pkg from './package.json';
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: `TimeHash.js`,
+      format: 'cjs'
+    },
+    {
+      file: `TimeHash.es.js`,
+      format: 'esm'
+    },
+    {
+      name: 'TimeHash',
+      file: `TimeHash.umd.js`,
+      format: 'umd'
+    }
+  ],
+  plugins: [
+     resolve(),
+      babel({
+        exclude: 'node_modules/**',
+        presets: [
+          [
+            '@babel/env',
+            {
+              modules: 'false',
+              targets: {
+                browsers: '> 1%, IE 11, not op_mini all, not dead',
+                node: 8
+              },
+              useBuiltIns: 'usage'
+            }
+          ]
+        ]
+      }),
+      commonjs(),
+      terser()
+  ]
+};
